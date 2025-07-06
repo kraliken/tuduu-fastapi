3f9d92bfa7a996037834ee1032f9a19f6467b62f
Kralik N <kraaliknorbert@gmail.com>
2025-07-06 20:41:09 +0200

    resolve ModuleNotFoundError for 'routers.upload'

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from contextlib import asynccontextmanager
 from routers.auth import authentication
 from routers.admin import users
 from routers.todo import todos
-from routers.upload import upload
+
+# from routers.upload import upload
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -41,4 +42,4 @@ app.add_middleware(
 app.include_router(authentication.router)
 app.include_router(users.router, prefix="/api/v1")
 app.include_router(todos.router, prefix="/api/v1")
-app.include_router(upload.router, prefix="/api/v1")
+# app.include_router(upload.router, prefix="/api/v1")
